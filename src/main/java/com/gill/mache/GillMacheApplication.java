113e6eb822b9c772318f04b74f1b5006646ba21c
912343041@qq.com <912343041@qq.com>
2023-08-03 15:43:28 +0800

    init

---
+++ src/main/java/com/gill/mache/GillMacheApplication.java
@@ -0,0 +1,17 @@
+package com.gill.mache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GillMacheApplication
+ * 
+ * @author gill
+ */
+@SpringBootApplication
+public class GillMacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GillMacheApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gill/mache/GillMacheApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gill.mache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GillMacheApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
